e5e2fccb863d6c33ba853e61f07454b079e0dc30
Sriram2828 <sriram101110001@gmail.com>
2023-11-22 14:01:24 +0530

    inserting the data into mysql database using php

--- phpWorks/insertIntoDB/connectdb.php
+++ phpWorks/insertIntoDB/connectdb.php
@@ -1,9 +1,10 @@
 <?php
-$servername = "localhost:3306";
-$username = "root";
-$password = "";
-$db_name = "studentsdb";
-$connect = new mysqli($servername, $username, $password, $db_name);
+//  run the php code and access the mysql database(phpmyadmin) using the xampp software 
+$servername = "localhost:3306"; 
+$username = "root"; // default username
+$password = ""; 
+$db_name = "studentsdb"; //use the database name
+$connect = new mysqli($servername, $username, $password, $db_name); //give the field names of the table
 
 // connection checking
 if($connect->connect_error){
--- phpWorks/insertIntoDB/index.php
+++ phpWorks/insertIntoDB/index.php
@@ -1,47 +1,71 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="index.css">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="index.css" />
     <title>validation</title>
-</head>
-<body>
+  </head>
+  <body>
     <form action="index.php" method="post">
-        <div class="Details">
-            <h1>Student details</h1>
+      <div class="Details">
+        <h1>Student details</h1>
 
-            <label for="stdname">Student name:</label>
-            <input type="text" placeholder="your name" name="stdname"  required>
-            <br>
+        <label for="stdname">Student name:</label>
+        <input type="text" placeholder="your name" name="stdname" required />
+        <br />
 
-            <label for="regno">Register number:</label>
-            <input type="text" placeholder="12-digit register number" name="regno"  required>
-            <br>
+        <label for="regno">Register number:</label>
+        <input
+          type="text"
+          placeholder="12-digit register number"
+          name="regno"
+          required
+        />
+        <br />
 
-            <label for="phno">phone number:</label>
-            <input type="text" placeholder="10-digit mobile number" name="phno" required>
-            <br>
-        
-            <label for="email">Email:</label>
-            <input type="email" placeholder="xxx...@gmail.com" name="email"  required>
-            <br>
+        <label for="phno">phone number:</label>
+        <input
+          type="text"
+          placeholder="10-digit mobile number"
+          name="phno"
+          required
+        />
+        <br />
 
-            <label for="pwd">Password:</label>
-            <input type="password" placeholder="enter your password" name="pwd"  required>
-            <br>
+        <label for="email">Email:</label>
+        <input
+          type="email"
+          placeholder="xxx...@gmail.com"
+          name="email"
+          required
+        />
+        <br />
 
-            <label for="confpwd">Confirm Password:</label>
-            <input type="password" placeholder="Retype your password" name="confpwd"  required>
-            <br>
-            
-            <div>
-                <input  type="submit" name="submit" value="Insert details">
-            </div>
-            
+        <label for="pwd">Password:</label>
+        <input
+          type="password"
+          placeholder="enter your password"
+          name="pwd"
+          required
+        />
+        <br />
+
+        <label for="confpwd">Confirm Password:</label>
+        <input
+          type="password"
+          placeholder="Retype your password"
+          name="confpwd"
+          required
+        />
+        <br />
+
+        <div class="btn">
+          <input type="submit" name="submit" value="Insert details" />
         </div>
-    </form> 
-</body>
+      </div>
+    </form>
+  </body>
 </html>
 <?php
 include("connectdb.php");
