b2ea14d1d9fba5237828ffca78a1cd2443ba1b29
Sriram2828 <sriram101110001@gmail.com>
2023-11-22 14:17:03 +0530

    link the MYSQL database to the dataform

--- phpWorks/insertIntoDB/connectdb.php
+++ phpWorks/insertIntoDB/connectdb.php
@@ -2,7 +2,7 @@
 //  run the php code and access the mysql database(phpmyadmin) using the xampp software 
 $servername = "localhost:3306"; 
 $username = "root"; // default username
-$password = ""; 
+$password = ""; // in default there is no password available for the database
 $db_name = "studentsdb"; //use the database name
 $connect = new mysqli($servername, $username, $password, $db_name); //give the field names of the table
 
