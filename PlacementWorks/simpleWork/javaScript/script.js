27b0e9a5af0934d4da9cb661ee997569f2699bc7
Sriram2828 <sriram101110001@gmail.com>
2024-07-12 17:20:09 +0530

    task 11/7/24

---
+++ PlacementWorks/simpleWork/javaScript/script.js
@@ -0,0 +1,20 @@
+// var a = 10;
+// let b = 10;
+// const c = 10;
+// function notify() {
+//     var a = 10;
+//     let b = 10+1;
+//     const c = 10;
+//     x = 10;
+//     // alert(c+x);
+//     console.log(b+x);
+// }
+// console.log(b);
+const std = {
+    fname: "xxx",
+    rollno: "100"
+};
+console.log(std);
+var n = "Sriram";
+var p = "Balamurugan";
+console.log(n+p);
