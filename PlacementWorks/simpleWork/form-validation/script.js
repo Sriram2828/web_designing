d6389d45d598ac2e7b5cbc86a1b2022a91f9b6a0
Sriram2828 <sriram101110001@gmail.com>
2024-07-20 13:03:42 +0530

    task 20/7/24

--- PlacementWorks/simpleWork/form-validation/script.js
+++ PlacementWorks/simpleWork/form-validation/script.js
@@ -1,9 +1,11 @@
+let personName = document.getElementById("name");
+let gender = document.getElementById("gender");
 let email = document.getElementById("email");
 let pwd = document.getElementById("pass");
 function validate(){
     if(email.value === "sam@gmail.com" && pwd.value === "123qweasd"){
-        alert("valid");
+        alert("name: "+personName.value+"\n"+"gender: "+gender.value+"\n"+personName.value+" is an authorized person");
     }else{
-        alert("not valid");
+        alert(personName.value+" is not an authorized person");
     }
 }
