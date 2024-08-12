2d35d84b92b7ce5c8a05e8bb61f3ebadfbf8ce23
Sriram2828 <sriram101110001@gmail.com>
2024-08-12 16:07:39 +0530

    Transferred the files to separate Repositories

--- PlacementWorks/ui-manupulation-using-js-Assignment-12/message-box/script.js
+++
@@ -1,9 +0,0 @@
-let msg = document.getElementById("msg");
-function accept() {
-    alert(msg.value+" is accepted👍🏻");
-    msg.value="";
-}
-function decline() {
-    alert(msg.value+" is declined❌");
-    msg.value="";
-}
